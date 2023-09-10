291db0b15721ce3a072d35da8a2bb5e33da9bbc9
Melhefnawi <>
2023-09-10 09:31:18 +0000

    all

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ['8000-melhefnawi-djangoproject-e2pwa7lc18.us2.codeanyapp.com',
     'jango-project.herokuapp.com', 'localhost']
 
 
